6963abf7819e38ee88a36350a5b9005c658c3133
geruny <gerunuky@gmail.com>
2021-05-05 23:34:45 +0300

    adaptive for home done

--- wp-content/themes/modern_child/child-home.php
+++ wp-content/themes/modern_child/child-home.php
@@ -165,12 +165,16 @@ while (have_posts()) : the_post();
         </section>
         <section id="contacts">
             <div class="container">
-                <div class="row wow fadeInUp contacts-entry">
-                    <h2>Наши контакты</h2>
+                <div class="row">
+                    <div class="col">
+                        <div class="title wow fadeInUp">
+                            <h2>Наши контакты</h2>
+                        </div>
+                    </div>
                 </div>
-                <div class="bg-contacts wow fadeInUp">
+                <div class="contacts-info wow fadeInUp">
                     <div class="row g-0 gy-3">
-                        <div class="col-12 col-md">
+                        <div class="col-12 col-md adres-wrapper">
                             <div class="adres">
                                 <h3>Адрес:</h3>
                                 <a href="https://goo.gl/maps/xPbg7G7c4N4nJ6rv8" target="_blank"><i class="fas fa-map-marker-alt"></i>Гражданская ул., 13-15, Санкт-Петербург</a>
@@ -185,7 +189,7 @@ while (have_posts()) : the_post();
                             <h3>Email:</h3>
                             <a href="mailto:asms-designspb@yandex.ru"><i class="fas fa-envelope"></i>asms-designspb@yandex.ru</a>
                         </div>
-                        <div class="col-12 col-md border-left">
+                        <div class="col-12 col-md border-left social">
                             <h3>Социальные сети:</h3>
                             <div class="social">
                                 <a href="https://www.behance.net/amdesignstudio2" target="_blank"><i class="fab fa-behance"></i></a>
@@ -195,47 +199,12 @@ while (have_posts()) : the_post();
                         </div>
                     </div>
                 </div>
-                <div class="row wow fadeInUp map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.2313212808774!2d30.31046581606245!3d59.92830368187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46963102f889cdc7%3A0xc5db47709b2b1513!2z0JPRgNCw0LbQtNCw0L3RgdC60LDRjyDRg9C7LiwgMTMtMTUsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTAwMzE!5e0!3m2!1sru!2sru!4v1614794246252!5m2!1sru!2sru" width="" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                </div>
+
+            </div>
+            <div class="row wow fadeInUp map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.2313212808774!2d30.31046581606245!3d59.92830368187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46963102f889cdc7%3A0xc5db47709b2b1513!2z0JPRgNCw0LbQtNCw0L3RgdC60LDRjyDRg9C7LiwgMTMtMTUsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTAwMzE!5e0!3m2!1sru!2sru!4v1614794246252!5m2!1sru!2sru" width="" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </section>
-        <!-- <section id="contacts-block" class="contact">
-            <div class="container">
-                <dirv class="row wow fadeInUp">
-                    <div class="col-md-4">
-                        <div class="info-box">
-                            <h2>Свяжитесь с нами</h2>
-                            <div class="row">
-                                <h3>Адрес:</h3>
-                                <a href="https://goo.gl/maps/xPbg7G7c4N4nJ6rv8" target="_blank"><i class="fas fa-map-marker-alt"></i>Гражданская ул., 13-15, Санкт-Петербург</a>
-                            </div>
-
-                            <div class="row">
-                                <h3>Телефоны:</h3>
-                                <a id="fist-phone" href="tel:+79817455467"><i class="fas fa-phone-alt"></i>+7(981)745-54-67</a>
-                                <a href="tel:+79818752531"><i class="fas fa-phone-alt"></i>+7(981)875-25-31</a>
-                            </div>
-                            <div class="row">
-                                <h3>Email:</h3>
-                                <a href="mailto:asms-designspb@yandex.ru"><i class="fas fa-envelope"></i>asms-designspb@yandex.ru</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.2313212808774!2d30.31046581606245!3d59.92830368187245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46963102f889cdc7%3A0xc5db47709b2b1513!2z0JPRgNCw0LbQtNCw0L3RgdC60LDRjyDRg9C7LiwgMTMtMTUsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTAwMzE!5e0!3m2!1sru!2sru!4v1614794246252!5m2!1sru!2sru" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                    </div>
-                </dirv>
-                <div class="row wow fadeInUp">
-                    <div class="social-links">
-                        <a href="#" class="facebook"><i class="fa fa-vk" aria-hidden="true"></i></a>
-                        <a href="#" class="instagram"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="#" class="google-plus"><i class="fa fa-behance" aria-hidden="true"></i></a>
-                        <a href="#" class="linkedin"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-            </div>
-        </section> -->
 
         <script>
             new WOW().init();
